b578e14b4e84ccfa0a0ee2a8fd586c6bb3a69bdf
maxgrossman <m.grossman1813@gmail.com>
2017-08-19 18:08:26 -0400

    init app;

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,5 +1,29 @@
 'use strict';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import reducer from './reducers/reducer';
 import config from './config';
 
-console.log.apply(console, config.consoleMessage);
-console.log('Environment', config.environment);
+const logger = createLogger({
+  level: 'info',
+  collapsed: true,
+  predicate: (getState, action) => {
+    return (config.environment !== 'production');
+  }
+});
+
+const finalCreateStore = compose(
+  applyMiddleware(logger, thunkMiddleware)
+)(createStore);
+
+const store = finalCreateStore(reducer);
+
+render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.querySelector('.site-canvas'));
